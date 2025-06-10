7d6e8d695402ab5666c7bbae9205bda1b86a5a66
Anastasiia Barinova <anastasiia.barinova@benify.com>
2025-06-10 16:00:58 +0200

    build color mode switcher

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from '@chakra-ui/react';
+import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/random.svg';
+import { ColorModeSwitch } from '@/components/ColorModeSwitch.tsx';
 
 export const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image boxSize="60px" src={logo}></Image>
-      <Text>NavBar</Text>
+      <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
 };
