857a7acd143cb9d45ab4e28fd6f8917e87623cde
Anastasiia Barinova <anastasiia.barinova@benify.com>
2025-06-14 13:49:34 +0200

    devtools

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx';
 import theme from '@/theme.ts';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient({});
 createRoot(document.getElementById('root')!).render(
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
       <ChakraProvider theme={theme}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
+        <ReactQueryDevtools></ReactQueryDevtools>
       </ChakraProvider>
     </QueryClientProvider>
   </StrictMode>,
